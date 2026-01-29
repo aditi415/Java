42301cc101b8c376107117ace0c850d5361397c4
Aditi Bhosure <bhosure.as2003@gmail.com>
2026-01-29 15:06:34 +0530

    hello.java

---
+++ Basic Concepts/Var-Data types/hello.java
@@ -0,0 +1,37 @@
+class hello
+{
+    public static void main(String[] args) {
+      //variable
+        float marks =3.5f;
+       byte by = 127;
+       short sh=553;
+       long l =33445l;
+
+       char c='w';
+       boolean bn =true;
+
+        System.out.println(marks);
+
+        //literals
+
+        int no=0b10011;
+        int no1=0x9A;
+
+        //type conversion and casting
+
+    float fl=5.6f;
+    int x=(int)f;
+
+    byte b = 127;
+    int a = b;
+    System.out.println(a);
+
+    int j=234;
+    byte k = (byte) j;
+    System.out.println(k);
+
+    float f = 5.6f;
+    int t =(int) f;
+    System.out.println(t);
+    }
+}
