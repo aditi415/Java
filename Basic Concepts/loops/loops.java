f6cedde59ac8b479b7ba107f3b4ba69214cf20b6
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-02-04 16:05:16 +0530

    Add files via upload

---
+++ Basic Concepts/loops/loops.java
@@ -0,0 +1,41 @@
+public class loops {
+    public static void main(String[] args){
+        //while
+        int i=1;
+        while (i<=4)
+        {
+            System.out.println("Hi" + i );
+            i++;
+
+            int j=1;
+            while(j<=3){
+                System.out.println("Hello");
+
+                j++;
+            }
+        }
+        System.out.println("Bye" + i);
+
+        //do while
+
+        int i =5;
+
+        do{
+            System.out.println("Hi" + i);
+            i++;
+
+        }while(i<=4);
+ 
+        //for loop
+        for(int k=1;k<=7;k++)
+         {
+            System.out.println("DAY" +  k);
+
+            for(int l=1;l<=9;l++)
+            {
+                System.out.println(" " +(l+8)+ "-" +(l+9));
+            }
+         }
+    }
+    
+}
