a98d9f89712c68b3c92648711cdeada58dd63e9b
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-02-11 09:57:37 +0530

    Add files via upload

---
+++ Basic Concepts/Encapsulation/A.java
@@ -0,0 +1,17 @@
+public class A{
+    public void show()
+    {
+        System.out.println("in class A");
+    }
+
+public static void main(String[] args)
+{
+    //int marks;
+    //marks=99;
+
+    //A obj=
+    new A();            //new A() - it is the object creation 
+    new A().show();
+    //obj.show();
+} 
+}
