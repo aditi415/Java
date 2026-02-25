b145c33218fdf1442f72f6c448b61ca97871b189
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-02-25 15:38:06 +0530

    Add files via upload

---
+++ Basic Concepts/Inheritance/multiple.java
@@ -0,0 +1,21 @@
+//java does not support multiple inheritance
+
+class A{
+
+}
+
+class B{
+
+}
+
+class C{
+
+}
+
+public class multiple            //multiple inheritance
+{
+    public static void main(String[] args)
+    {
+
+    }    
+}
