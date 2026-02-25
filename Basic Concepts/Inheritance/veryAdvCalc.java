7e54aef3980865fa4eaa286e2c034d5f5c0c55f8
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-02-25 15:35:36 +0530

    Add files via upload

---
+++ Basic Concepts/Inheritance/veryAdvCalc.java
@@ -0,0 +1,8 @@
+public class veryAdvCalc extends Calc   //multi level inheritance i.e veryAdvCalc->Advcal->Calc
+{
+    public double power(int n9, int n10)
+    {
+        return Math.pow(n9,n10);
+    }
+    
+}
