f273502d3d97756d87f1c4e9006a3bf0c7d1ca95
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-02-27 10:54:12 +0530

    Add files via upload

---
+++ Basic Concepts/Inheritance/AdvCalc.java
@@ -0,0 +1,13 @@
+
+public class AdvCalc extends Calc     //AdvCalc is subclass Calc is superclass extends is inheriting property of another class
+{
+    public int multi(int n5,int n6)
+    {
+        return n5*n6;
+    }
+
+    public int div(int n7,int n8)
+    {
+        return n7/n8;
+    }
+}
