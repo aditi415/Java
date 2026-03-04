0d79d75825eaf281bf75a544932d21fec6f1d462
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-03-04 11:07:19 +0530

    Add files via upload

---
+++ Basic Concepts/Arrays/lists.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class lists {
+    public static void main(String[] args) {
+        List<Object> arr = new ArrayList<>();
+
+        int[] arr1={1,2,3};
+      
+        for (int i=0;i<arr1.length;i++){
+            System.out.println(arr1[i]);
+        }
+    }
+}
