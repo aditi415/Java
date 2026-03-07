56fc3b617eab1b1a302f33803c17c858fb2f330a
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-03-07 13:24:00 +0530

    Add files via upload

---
+++ Basic Concepts/Annotation/notation.java
@@ -0,0 +1,23 @@
+/*
+    Annotaion is a suppliment to a compiler or runtime or also called a Meta data 
+*/
+class A{
+    public void show(){
+        System.out.println("in A show");
+    }
+}
+        //override method of A in B 
+class B extends A{
+    @Override
+    public void show(){
+        System.out.println("in B show");
+    }
+}
+
+public class notation {
+    public static void main(String[] args) {
+        B obj =new B();
+        obj.show();
+
+    }
+}
