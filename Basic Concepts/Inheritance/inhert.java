70334bf8335705612e5b74cafedfda15b94af018
Aditi Bhosure <177236763+aditi415@users.noreply.github.com>
2026-03-21 12:39:27 +0530

    inhert.java

--- Basic Concepts/Inheritance/inhert.java
+++ Basic Concepts/Inheritance/inhert.java
@@ -1,4 +1,4 @@
- /*subclass (child) - the class that inherits from another class
+/*subclass (child) - the class that inherits from another class
 superclass (parent) - the class being inherited from*/
 
 package inheritance;
